37c7369519d6c9e198901c1d684421321df7279d
diogoscatro <diogocas89@gmail.com>
2026-02-24 21:16:30 -0300

    chore: news templates and updates in views, urls and settings

--- cars/views.py
+++ cars/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search=request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+    
     return render(request, 'cars.html', {'cars': cars})
