f954568f5d4892db6652dee6074a3841d744a24a
diogoscatro <diogocas89@gmail.com>
2026-03-05 20:21:14 -0300

    feat: add app accounts

--- cars/forms.py
+++ cars/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-from .models import Brand
+from .models import Car
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=100, required=True)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField(required=True)
-    model_year = forms.IntegerField(required=True)
-    plate = forms.CharField(max_length=10, required=False)
-    value = forms.DecimalField(max_digits=10, decimal_places=2, required=True)
-    photo = forms.ImageField(required=False)
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 0:
+            raise forms.ValidationError("O valor não pode ser negativo.")
+        return value
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1886:  # O primeiro carro foi inventado em 1886
+            raise forms.ValidationError("O ano de fabricação não pode ser anterior a 1886.")
+        return factory_year
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -12,5 +12,11 @@ def cars_view(request):
     return render(request, 'cars.html', {'cars': cars})
 
 def new_car_view(request):
-    new_car_form = CarForm()
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
     return render(request, 'new_car.html', {'new_car_form': new_car_form})
