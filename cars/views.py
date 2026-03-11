3688fb1ee8753a4f5134333604609f082c633f1d
diogoscatro <diogocas89@gmail.com>
2026-03-10 22:42:22 -0300

    refactor: update views for car creation and routing

--- cars/views.py
+++ cars/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 from cars.models import Car
 from cars.forms import CarModelForm
+  
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search=request.GET.get('search')
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
-    
-    return render(request, 'cars.html', {'cars': cars})
+        if search:
+            cars = cars.filter(model__icontains=search)
+        
+        return cars
 
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = reverse_lazy('cars_list')
